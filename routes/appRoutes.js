cbfa0ffbf02369bb50980c0a0cf8837ae11114cb
Facundo <facundomoriconi19@gmail.com>
2024-09-11 21:56:04 -0300

    commit

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -15,27 +15,27 @@ const router = express.Router()
  router.get('/clientes/:id', authenticateToken, getClienteId)
 
  // Actualizar clientes
- router.put('/clientes/:id', uptadeCliente)
+ router.put('/clientes/:id', authenticateToken, uptadeCliente)
 
  // eliminar cliente
- router.delete('/clientes/:id', deleteCliente)
+ router.delete('/clientes/:id', authenticateToken, deleteCliente)
 
  // Productos
- router.post('/productos', subirImagen, nuevoProducto)
- router.get('/productos', getProductos)
- router.get('/productos/:id', getProductoId)
- router.put('/productos/:id', subirImagen, updateProducto)
- router.delete('/productos/:id', deleteProducto)
+ router.post('/productos',  authenticateToken, subirImagen, nuevoProducto)
+ router.get('/productos',  authenticateToken, getProductos)
+ router.get('/productos/:id',  authenticateToken, getProductoId)
+ router.put('/productos/:id',  authenticateToken, subirImagen, updateProducto)
+ router.delete('/productos/:id', authenticateToken, deleteProducto)
 
  // Busqueda de productos
- router.post('/productos/busqueda/:query', buscarProducto)
+ router.post('/productos/busqueda/:query', authenticateToken, buscarProducto)
 
  // Pedidos
- router.post('/pedidos/nuevo/:id', nuevoPedido)
- router.get('/pedidos', getPedidos)
- router.get('/pedidos/:id', getPedidosId)
- router.put('/pedidos/:id', updatePedido)
- router.delete('/pedidos/:id', deletePedido)
+ router.post('/pedidos/nuevo/:id',  authenticateToken, nuevoPedido)
+ router.get('/pedidos',  authenticateToken, getPedidos)
+ router.get('/pedidos/:id', authenticateToken, getPedidosId)
+ router.put('/pedidos/:id', authenticateToken, updatePedido)
+ router.delete('/pedidos/:id', authenticateToken, deletePedido)
 
  // Usuarios
  router.post('/usuarios/nuevo', nuevoUsuario)
